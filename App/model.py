4bb60be5aac073ab29550cde6718cde36b399541
ufsoko <1085699796@qq.com>
2020-01-06 17:48:26 +0800

    flask学习模板

--- App/model.py
+++ App/model.py
@@ -5,7 +5,9 @@ class User(db.Model):
     name = db.Column(db.String(50))
     title = db.Column(db.String(25))
     body = db.Column(db.Text(250))
-
+    create_time = db.Columu(db.DateTime)
+    # testt
+    # 哈哈哈
     def save(self):
         db.session.add(self)
         db.session.commit()
